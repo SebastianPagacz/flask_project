15bfdeecc6cfc36d15dcf5e5b6d2f2bc140e03cf
SebastianPagacz <234595@student.uek.krakow.pl>
2024-06-10 21:15:54 +0200

    Basic tmplates

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
 #in this file we wanna put everything that is realted to navigation of the site
-from flask import Blueprint
+from flask import Blueprint, render_template
 #first variable defines name of the blueprint
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
 # this function will run whenever user goes to /
-    return "<h1>test</h1>"
+    return render_template("home.html")
